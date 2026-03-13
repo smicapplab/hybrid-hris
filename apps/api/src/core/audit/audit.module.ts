7b5bbfffa4051ee05d7a8ea538393f42cea0e48e
Steve Torrefranca <s.torrefranca@gmail.com>
2026-03-14 02:42:15 +0800

    fix(api): add missing DatabaseModule import to AuditModule

--- apps/api/src/core/audit/audit.module.ts
+++ apps/api/src/core/audit/audit.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { AuditService } from './audit.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Global()
 @Module({
+  imports: [DatabaseModule],
   providers: [AuditService],
   exports: [AuditService],
 })
