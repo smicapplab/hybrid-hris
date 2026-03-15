40446163ac8751004f8629e8313af77a97187c80
Steve Torrefranca <s.torrefranca@gmail.com>
2026-03-15 23:28:05 +0800

    chore: rerun reset db

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -13,6 +14,7 @@ import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     AuthModule,
     UsersModule,
     DatabaseModule,
