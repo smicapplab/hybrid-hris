8e3956bea335d306c5a48d2c963122a459ce62a5
Steve Torrefranca <s.torrefranca@gmail.com>
2026-04-10 00:14:53 +0800

    Update ecosystem.config.js

--- apps/api/ecosystem.config.js
+++ apps/api/ecosystem.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Load environment variables from the root .env file (two levels up)
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
 module.exports = {
   apps: [
     {
       name: 'be-hris-api',
       script: 'dist/main.js',
-      cwd: '/home/ec2-user/Projects/hybrid-hris/apps/api',
+      cwd: './',
       instances: 1,
       exec_mode: 'fork',
       watch: false,
@@ -13,6 +19,7 @@ module.exports = {
       merge_logs: true,
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
       env: {
+        ...process.env,
         NODE_ENV: 'production',
         PORT: '4000',
         NODE_OPTIONS:
